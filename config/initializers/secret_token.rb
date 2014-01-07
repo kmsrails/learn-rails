4c99df54a408678e280d1366c010061431f33b81
kmsrails <user@ubuntu.(none)>
2014-01-07 01:17:24 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearnRails::Application.config.secret_token = '4f8a1b73f63cf0154784f2cb465dc29234b462f4ff5233fcebcbd5b0d24e7c0071dfce9453307caa3a74186390a18ec663b546e81e364d1a4d1289f8150950f0'
